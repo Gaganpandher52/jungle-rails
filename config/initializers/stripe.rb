28aed2fb3fca0cdeb0d50e666088b5e540690bd7
Gaganpandher52 <gaganpandher52@gmail.com>
2019-06-12 19:58:45 +0000

    first commit

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,7 @@ Rails.configuration.stripe = {
   :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
+puts Rails.configuration.stripe[:publishable_key]
+
+# Stripe.publishable_key = Rails.configuration.stripe[:publishable_key]
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
