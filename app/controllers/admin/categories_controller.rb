376c4efff804ab45d15ffd3b3e3a2bb473c6b000
Gaganpandher52 <gaganpandher52@gmail.com>
2019-06-13 16:58:14 +0000

    added delete button to the category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,6 +18,11 @@ class Admin::CategoriesController < ApplicationController
       render :new
     end
   end
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
   private
 
   def category_params
