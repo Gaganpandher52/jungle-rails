eaa978159728348caf6006ced2b41f69bf292d81
Gaganpandher52 <gaganpandher52@gmail.com>
2019-06-17 20:45:23 +0000

    refactore the testing code

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -4,26 +4,29 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'should have a name' do
       @newCategory = Category.new name:'Sports'
-      @newPro = Product.new name:'new',price:129.99, quantity:15,category:@newCategory
+      expect(@newCategory.name).to be_present
+       
+    end
+    it 'should have a name' do
+      @newPro = Product.new name:'new'
       expect(@newPro.name).to be_present
        
     end
     it 'should have a name' do
-      @newCategory = Category.new name:'Sports'
-      @newPro = Product.new name:'new',price:129.99, quantity:15,category:@newCategory
+      
+      @newPro = Product.new price:129.99
       expect(@newPro.price).to be_present  
       
     end
     it 'should have a name' do
-      @newCategory = Category.new name:'Sports'
-      @newPro = Product.new name:'new',price:129.99, quantity:15,category:@newCategory
+      @newPro = Product.new quantity:15
       expect(@newPro.quantity).to be_present  
       
     end
     it 'should have a name' do
       @newCategory = Category.new name:'Sports'
-      @newPro = Product.new name:'new',price:129.99, quantity:15,category:@newCategory
-      expect(@newPro.category).to be_nil   
+      @newPro = Product.new category:@newCategory
+      expect(@newPro.category).to be_present   
     end
 
   end
